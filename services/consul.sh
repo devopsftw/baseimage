9923c53014335174293944502ce4ca57fea2ee01
Alex Salt <alex.salt@e96.ru>
2016-10-23 19:22:42 +0500

    use consul 0.7

--- services/consul.sh
+++ services/consul.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-_term() {
-    kill -INT $pid
-    wait $pid
-}
-# trap term for gracefully stopping consul
-trap _term TERM
-
-/usr/local/bin/consul agent \
+exec /usr/local/bin/consul agent \
     -join $CONSUL_HOST \
     -dc $CONSUL_DC \
     -domain $CONSUL_DOMAIN \
     -config-file /etc/consul/consul.json \
     -config-dir /etc/consul/conf.d &
-pid=$!
-wait $pid
