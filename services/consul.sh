47c18aa47c295a9fe7cc7e47cfec907cdabdec93
Alex Salt <alex.salt@e96.ru>
2016-10-24 16:20:28 +0500

    update consul startup script

--- services/consul.sh
+++ services/consul.sh
@@ -2,7 +2,7 @@
 
 exec /usr/local/bin/consul agent \
     -join $CONSUL_HOST \
-    -dc $CONSUL_DC \
+    -datacenter $CONSUL_DC \
     -domain $CONSUL_DOMAIN \
     -config-file /etc/consul/consul.json \
-    -config-dir /etc/consul/conf.d &
+    -config-dir /etc/consul/conf.d
